f1c9c79f97bb289f5468a666d6be9d128ac19231
harshit <hkmeharban@gmail.com>
2026-03-15 17:26:27 +0530

    jjjj

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -67,7 +67,7 @@ const HomePage = () => {
             transition={{ duration: 0.5, ease: [0.25, 0.1, 0.25, 1] }}
             className="font-serif text-4xl font-semibold leading-snug tracking-tight text-foreground md:text-5xl"
           >
-            Is This Vegan?
+            Wondering if your Indian Snack is Vegan or Not?
           </motion.h1>
           <motion.p
             initial={{ opacity: 0, y: 20 }}
